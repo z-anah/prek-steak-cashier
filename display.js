a9f819f5bcddf24b267526fe69ab6f9e8415481c
anah <40556052+z-anah@users.noreply.github.com>
2024-01-09 00:56:49 +0700

    calculate

--- display.js
+++ display.js
@@ -6,6 +6,7 @@ const displayPsMenu = () => {
   generateTableHead(table, data);
   generateTable(table, display_ps_menu);
 };
+
 const generateTableHead = (table, data) => {
   let thead = table.createTHead();
   let row = thead.insertRow();
@@ -16,6 +17,7 @@ const generateTableHead = (table, data) => {
     row.appendChild(th);
   }
 };
+
 const generateTable = (table, data) => {
   for (let element of data) {
     let row = table.insertRow();
@@ -27,11 +29,82 @@ const generateTable = (table, data) => {
     text.setAttribute("class", "text-right");
     text.appendChild(document.createTextNode(element.harga));
     cell.appendChild(text);
+    // add button +
+    cell = row.insertCell();
+    let btn = document.createElement("button");
+    btn.setAttribute("class", "btn btn-sm btn-primary");
+    btn.setAttribute("type", "button");
+    btn.setAttribute(
+      "onclick",
+      "add_ps_order_menu('" + element.label + "'," + element.harga + ")"
+    );
+    btn.setAttribute("data-toggle", "modal");
+    btn.setAttribute("data-target", "#ps-order-menu-modal");
+    btn.setAttribute("data-ps-menu-id", element.id);
+    btn.appendChild(document.createTextNode("+"));
+    cell.appendChild(btn);
   }
+};
+
+let local_ps_order_menu = [];
+const add_ps_order_menu = (label, harga) => {
+  local_ps_order_menu.push({
+    label: label,
+    harga: harga,
+  });
+  displayPsOrderMenu();
+  calculateTotal();
+  calculateKembalian();
+};
+
+const displayPsOrderMenu = () => {
+  let table = document.getElementById("ps-order-menu-table");
+  table.innerHTML = "";
+  let data = Object.keys(local_ps_order_menu[0]);
+  generateTableHead(table, data);
+  generateTable(table, local_ps_order_menu);
+};
+const calculateTotal = () => {
+  let total = 0;
+  for (let element of local_ps_order_menu) {
+    total += element.harga;
+  }
+  document.getElementById("ps-total").innerHTML = total;
+};
+const calculateKembalian = () => {
+  let total = document.getElementById("ps-total").innerHTML;
+  let bayar = document.getElementById("ps-uang").value;
+  let kembalian = bayar - total;
+  document.getElementById("ps-kembalian").innerHTML = kembalian;
+};
+const input_ps_uang = (zero) => {
+  if (zero) {
+    if (zero == 1000) {
+      document.getElementById("ps-uang").value =
+      document.getElementById("ps-uang").value * zero;
+    } else {
+      let uangg = document.getElementById("ps-uang").value+""+zero;
+      // uangg to number
+      document.getElementById("ps-uang").value = uangg;
+    }
+  }
+  calculateKembalian();
+};
+const ulangi_ps_order_menu = () => {
+  local_ps_order_menu = [];
+  let table = document.getElementById("ps-order-menu-table");
+  table.innerHTML = "";
+  let ps_uang = document.getElementById("ps-uang");
+  ps_uang.value = "";
+  calculateTotal();
+  calculateKembalian();
 };
 
 const callAll = async () => {
   display_ps_menu = await get_ps_menu();
   displayPsMenu();
+
+  let huhu = await get_ps_order_menu_by_ps_order_id("bsCcjn3gKYUdb2g1yvCf");
 };
+
 callAll();
--- index.js
+++ index.js
@@ -9,24 +9,26 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const collection_ps_menu = db.collection("ps_menu");
+const collection_ps_menu_order = db.collection("ps_order_menu");
 let display_ps_menu = [];
 
 const get_ps_menu = async () => {
-  let ps_menu = [];
+  let val = [];
   try {
-    const querySnapshot = await collection_ps_menu.get();
+    const querySnapshot = await collection_ps_menu.orderBy("label").get();
     querySnapshot.forEach((doc) => {
-      ps_menu.push(doc.data());
+      val.push(doc.data());
     });
-    return ps_menu;
+    return val;
   } catch (error) {
     console.error("Error getting documents: ", error);
     alert(error);
   }
 };
+
 const add_ps_menu = async () => {
-    let label = document.getElementById("ps-menu-label").value;
-    let harga = document.getElementById("ps-menu-harga").value;
+  let label = document.getElementById("ps-menu-label").value;
+  let harga = document.getElementById("ps-menu-harga").value;
   try {
     await collection_ps_menu.doc().set({
       label: label,
@@ -34,6 +36,22 @@ const add_ps_menu = async () => {
     });
   } catch (error) {
     console.error("Error adding document: ", error);
+    alert(error);
+  }
+};
+
+const get_ps_order_menu_by_ps_order_id = async (ps_order_id) => {
+  let val = [];
+  try {
+    const querySnapshot = await collection_ps_menu_order
+      .where("ps_order_id", "==", ps_order_id)
+      .get();
+    querySnapshot.forEach((doc) => {
+      val.push(doc.data());
+    });
+    return val;
+  } catch (error) {
+    console.error("Error getting documents: ", error);
     alert(error);
   }
 };
